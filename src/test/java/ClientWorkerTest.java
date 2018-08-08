ee6f05cfd2d9e74f95db2633d8396e39b5be2784
liketic <ke.like@outlook.com>
2018-08-08 18:29:57 +0800

    Refactor test

--- src/test/java/ClientWorkerTest.java
+++ src/test/java/ClientWorkerTest.java
@@ -5,65 +5,41 @@ import com.aliyun.openservices.loghub.client.exceptions.LogHubClientWorkerExcept
 
 public class ClientWorkerTest {
 
-	public static void main(String[] args) throws LogHubClientWorkerException,
-			InterruptedException {
-		LogHubConfig config1 = new LogHubConfig(
-				"consumer_group_client_worker_test", "consumer_0",
-				"cn-hangzhou-failover-intranet.sls.aliyuncs.com",
-				"ali-cn-failover-sls-admin", "sls_operation_log",
-				"", "",
-				LogHubCursorPosition.BEGIN_CURSOR, 20 * 1000, false), config2 = new LogHubConfig(
-				"consumer_group_client_worker_test", "consumer_1",
-				"cn-hangzhou-failover-intranet.sls.aliyuncs.com",
-				"ali-cn-failover-sls-admin", "sls_operation_log",
-				"", "",
-				LogHubCursorPosition.BEGIN_CURSOR, 20 * 1000, false), config3 = new LogHubConfig(
-				"consumer_group_client_worker_test", "consumer_2",
-				"cn-hangzhou-failover-intranet.sls.aliyuncs.com",
-				"ali-cn-failover-sls-admin", "sls_operation_log",
-				"", "",
-				LogHubCursorPosition.BEGIN_CURSOR, 20 * 1000, false), config4 = new LogHubConfig(
-				"consumer_group_client_worker_test", "consumer_3",
-				"cn-hangzhou-failover-intranet.sls.aliyuncs.com",
-				"ali-cn-failover-sls-admin", "sls_operation_log",
-				"", "",
-				LogHubCursorPosition.BEGIN_CURSOR, 20 * 1000, false), config5 = new LogHubConfig(
-				"consumer_group_client_worker_test", "consumer_4",
-				"cn-hangzhou-failover-intranet.sls.aliyuncs.com",
-				"ali-cn-failover-sls-admin", "sls_operation_log",
-				"","",
-				LogHubCursorPosition.BEGIN_CURSOR, 20 * 1000, false);
+    private static final String TEST_PROJECT = "";
+    private static final String TEST_ENDPOINT = "";
+    private static final String TEST_LOGSTORE = "logstore-test";
+    private static final String ACCESS_KEY_ID = "";
+    private static final String ACCESS_KEY = "";
 
-		ClientWorker worker1 = new ClientWorker(
-				new LogHubProcessorTestFactory(), config1), worker2 = new ClientWorker(
-				new LogHubProcessorTestFactory(), config2), worker3 = new ClientWorker(
-				new LogHubProcessorTestFactory(), config3), worker4 = new ClientWorker(
-				new LogHubProcessorTestFactory(), config4), worker5 = new ClientWorker(
-				new LogHubProcessorTestFactory(), config5);
+    public static void main(String[] args) throws LogHubClientWorkerException,
+            InterruptedException {
 
-		Thread thread1 = new Thread(worker1), thread2 = new Thread(worker2), thread3 = new Thread(
-				worker3), thread4 = new Thread(worker4), thread5 = new Thread(worker5);
-		thread1.start();
-		thread2.start();
-		thread3.start();
-		thread4.start();
-		thread5.start();
+        int n = 5;
+        Thread[] threads = new Thread[n];
+        ClientWorker[] workers = new ClientWorker[n];
 
-		//*
-		Thread.sleep(60 * 60 * 1000);
+        for (int i = 0; i < n; i++) {
+            LogHubConfig config = new LogHubConfig(
+                    "consumer_group_client_worker_test", "consumer_" + i,
+                    TEST_ENDPOINT, TEST_PROJECT, TEST_LOGSTORE,
+                    ACCESS_KEY_ID, ACCESS_KEY,
+                    LogHubCursorPosition.BEGIN_CURSOR, 20 * 1000, false);
+            ClientWorker worker = new ClientWorker(new LogHubProcessorTestFactory(), config);
+            threads[i] = new Thread(worker);
+            workers[i] = worker;
+        }
+        for (int i = 0; i < n; i++) {
+            threads[i].start();
+        }
 
-		worker1.shutdown();
+        //*
+        Thread.sleep( 60 * 1000);
 
-		
-		//*/
-		Thread.sleep(12 * 60 * 60 * 1000);
+        for (int i = 0; i < n; i++) {
+            workers[i].shutdown();
+        }
 
-		worker2.shutdown();
-		worker3.shutdown();
-		worker4.shutdown();
-		worker5.shutdown();
-
-		Thread.sleep(60 * 1000);
-	}
+        Thread.sleep(60 * 1000);
+    }
 
 }
